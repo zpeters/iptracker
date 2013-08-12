2a28bad29146896c391792b26136ee359362d7d0
Zach Peters <zpeters@gmail.com>
2013-08-12 11:55:23 -0500

    Initial Import

---
+++ iptracker.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"os"
+)
+
+
+func main() {
+	//ipAddress := "8.8.8.8"
+	ipAddress := os.Args[1]
+	url := fmt.Sprintf("http://www.geoplugin.net/json.gp?ip=%s", ipAddress)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Couldn't make http connection\n")
+	}
+	defer resp.Body.Close()
+	body, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		log.Fatalf("Couldn't read body\n")
+	}
+
+	var objmap map[string]*json.RawMessage
+	err3 := json.Unmarshal(body, &objmap)
+	if err3 != nil {
+		log.Fatalf("Error unmarshalling: %s\n", err3)
+		log.Fatalf("%+v", objmap)
+	} 
+
+	// get the contry and country code
+	var cc string
+	var cn string
+	err = json.Unmarshal(*objmap["geoplugin_countryCode"], &cc)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	}
+	err = json.Unmarshal(*objmap["geoplugin_countryName"], &cn)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err)
+	}
+
+	if cc != "US" {	
+		fmt.Printf("IP: %s not in the US\n", ipAddress)
+		fmt.Printf("Results...\n")
+		fmt.Printf(string(body))
+	}
+}
